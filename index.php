f41ccb84cc3c503c8b56f2bf9cf8d29b901569e6
kresnoos <kresno.livingingarut@gmail.com>
2022-09-22 11:08:46 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,276 @@
+<!DOCTYPE html>
+<html lang="en">
+       <head>
+		<meta charset="UTF-8">
+		<meta name="keywords" content="Dinas Perumahan dan Permukiman Kabupaten Garut">
+		<meta name="author" content="creativegigs">
+		<meta name="description" content="Landing Page Dinas Perumahan dan Permukiman Kabupaten Garut">
+		<meta name='og:image' content='images/home/ogg.png'>
+		<!-- For IE -->
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<!-- For Resposive Device -->
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<!-- For Window Tab Color -->
+		<!-- Chrome, Firefox OS and Opera -->
+		<meta name="theme-color" content="#233D63">
+		<!-- Windows Phone -->
+		<meta name="msapplication-navbutton-color" content="#233D63">
+		<!-- iOS Safari -->
+		<meta name="apple-mobile-web-app-status-bar-style" content="#233D63">
+		<title>Dinas Perumahan dan Permukiman Kabupaten Garut</title>
+		<!-- Favicon -->
+		<link rel="icon" type="image/png" sizes="32x32" href="asset/fav.png">
+    <!-- vendor style sheet -->
+    <link rel="stylesheet" type="text/css" href="asset/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="asset/css/menu.css">
+    <link rel="stylesheet" type="text/css" href="asset/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="asset/css/owl.carousel.css">
+    <link rel="stylesheet" type="text/css" href="asset/css/owl.theme.css">
+    <link rel="stylesheet" type="text/css" href="asset/css/animate.css">
+    <link rel="stylesheet" type="text/css" href="asset/css/aos.css">
+    <link rel="stylesheet" type="text/css" href="asset/css/jquery.fancybox.min.css">
+    <link rel="stylesheet" type="text/css" href="asset/css/snazzy-info-window.min.css">
+    <link rel="stylesheet" type="text/css" href="asset/css/flaticon.css">
+    <link rel="stylesheet" type="text/css" href="asset/css/custom-animation.css">
+    <link rel="stylesheet" type="text/css" href="asset/css/animated_style.css">
+    <link rel="stylesheet" type="text/css" href="asset/css/iziModal.css">
+    <link rel="stylesheet" type="text/css" href="asset/css/jquery-ui.min.css">
+    <link rel="stylesheet" type="text/css" href="asset/css/selectize.css">
+    <link rel="stylesheet" type="text/css" href="asset/css/tabs.css">
+    
+    
+    
+    <!-- Main style sheet -->
+		<link rel="stylesheet" type="text/css" href="asset/css/style.css">
+		<!-- responsive style sheet -->
+		<link rel="stylesheet" type="text/css" href="asset/css/responsive.css">
+
+    
+	</head>
+    <style>
+        .kl{
+            cursor: pointer;
+        }
+        </style>
+	<body>
+		<div class="main-page-wrapper">
+			<section>
+				<div id="preloader">
+					<div id="ctn-preloader" class="ctn-preloader">
+						<div class="animation-preloader">
+							<div class="spinner"></div>
+							<div class="txt-loading">
+								<span data-text-preloader="S" class="letters-loading">
+									S
+								</span>
+								<span data-text-preloader="I" class="letters-loading">
+									I
+								</span>
+								<span data-text-preloader="G" class="letters-loading">
+									G
+								</span>
+								<span data-text-preloader="A" class="letters-loading">
+									A
+								</span>
+								<span data-text-preloader="P" class="letters-loading">
+									P
+								</span>
+							</div>
+						</div>	
+					</div>
+				</div>
+			</section>
+
+
+			<div id="theme-banner-two">
+        <div class="seo-what-we-do">
+          <div class="container">
+            <div class="row">
+              <div class="col-sm-3">
+                <div class="row" style="padding: 100px 0 0;">
+                  <div class="col-sm-12" style="text-align: center;">
+                    <img src="asset/kabgarut.png" alt="" style="height: 100px; display: inline-block;margin-right:15px">
+                    <img src="asset/logo.png" alt="" style="height: 100px; display: inline-block;">
+                  </div>
+                  <div class="col-sm-12">
+                    <h1 class="main-title wow fadeInUp animated" data-wow-delay="0.4s" style="text-align: center;text-decoration:none">SIAGA PRO PERKIM</h1>
+                    <p class="sub-title wow fadeInUp animated" data-wow-delay="0.9s">Dinas Perumahan dan Permukiman Kabupaten Garut </p>
+                  </div>
+                </div>
+              </div>
+              <div class="col-sm-9" style="z-index:10">
+                <div class="row" style="margin-bottom:100px">
+                  <div class="col-lg-3 col-sm-6 single-block" data-aos="fade-up">
+                    <div class="wrapper">
+                      <a href="https://disperkim.garutkab.go.id/portal"><div class="icon-box"><img src="asset/icon/icon50.png" alt=""></div></a>
+                      <a href="https://disperkim.garutkab.go.id/portal" class="title">Website</a>
+                    </div> <!-- /.wrapper -->
+                  </div> <!-- /.single-block -->
+                  
+									<div class="col-lg-3 col-sm-6 single-block" data-aos="fade-up" data-aos-delay="500">
+                    <div class="wrapper">
+										  <a href="https://disperkim.garutkab.go.id/sigap">
+                        <div class="icon-box"><img src="asset/icon/icon51.png" alt=""></div>
+                        <a href="https://disperkim.garutkab.go.id/sigap" class="title">Sigap Bencana</a>
+                      </a>
+                    </div> 
+                  </div>
+
+                  <div class="col-lg-3 col-sm-6 single-block" data-aos="fade-up" data-aos-delay="400">
+                    <div class="wrapper">
+											<a href="https://disperkim.garutkab.go.id/psu">
+                        <div class="icon-box"><img src="asset/icon/icon53.png" alt=""></div>
+                        <a href="https://disperkim.garutkab.go.id/psu" class="title">Penyerahan PSU</a>
+                      </a>
+                    </div> <!-- /.wrapper -->
+                  </div> <!-- /.single-block -->
+                  
+                  <div class="col-lg-3 col-sm-6 single-block" data-aos="fade-up" data-aos-delay="500">
+                    <div class="wrapper">
+											<a href="https://disperkim.garutkab.go.id/rutilahu">
+                        <div class="icon-box"><img src="asset/icon/icon52.png" alt=""></div>
+                        <a href="https://disperkim.garutkab.go.id/rutilahu" class="title">Rutilalhu</a>
+                      </a>
+                    </div> 
+                  </div> 
+                  
+                  <div class="col-lg-3 col-sm-6 single-block" data-aos="fade-up" data-aos-delay="300">
+                    <div class="wrapper">
+                      <a href="https://disperkim.garutkab.go.id/proposal">
+											  <div class="icon-box"><img src="asset/icon/icon48.png" alt=""></div>
+											  <a href="https://disperkim.garutkab.go.id/proposal" class="title">Pengajuan Usulan </a>
+                      </a>
+                    </div> <!-- /.wrapper -->
+                  </div> <!-- /.single-block -->
+
+                  <div class="col-lg-3 col-sm-6 single-block" data-aos="fade-up" data-aos-delay="500">
+                    <div class="wrapper">
+										  <a href="https://disperkim.garutkab.go.id/track">
+                        <div class="icon-box"><img src="asset/icon/icon6.svg" alt=""></div>
+                        <a href="https://disperkim.garutkab.go.id/track" class="title">Tracking Bantuan</a>
+                      </a>
+                    </div> 
+                  </div>
+
+									<div class="col-lg-3 col-sm-6 single-block" data-aos="fade-up" data-aos-delay="500">
+                    <div class="wrapper">
+											<a href="https://disperkim.garutkab.go.id/eoffice">
+                        <div class="icon-box"><img src="asset/icon/icon2.png" alt=""></div>
+                        <a href="https://disperkim.garutkab.go.id/eoffice" class="title">eOffice</a>
+                      </a>
+                    </div> 
+                  </div>
+
+                  <div class="col-lg-3 col-sm-6 single-block" data-aos="fade-up" data-aos-delay="500">
+                    <div class="wrapper">
+											<a href="#">
+                        <div class="icon-box"><img src="asset/icon/icon11.png" alt=""></div>
+                        <a href="#" class="title">Help Desk</a>
+                      </a>
+                    </div> 
+                  </div>
+
+                </div>
+              </div>
+            </div>
+          </div> <!-- /.container -->
+        </div> <!-- /.seo-what-we-do -->
+                
+			</div> <!-- /#theme-banner-two -->
+			
+	        <!-- Scroll Top Button -->
+			<button class="scroll-top tran3s">
+				<i class="fa fa-angle-up" aria-hidden="true"></i>
+			</button>
+			
+
+
+		<!-- Optional JavaScript _____________________________  -->
+
+    	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    	<!-- jQuery -->
+		<script src="asset/js/jquery.2.2.3.min.js"></script>
+		<!-- Popper js -->
+		<script src="asset/js/popper.min.js"></script>
+		<!-- Bootstrap JS -->
+		<script src="asset/js/bootstrap.min.js"></script>
+	    <!-- menu  -->
+		<script src="asset/js/custom.js"></script>
+		<!-- AOS js -->
+		<script src="asset/js/aos.js"></script>
+		<!-- WOW js -->
+		<script src="asset/js/wow.min.js"></script>
+		<!-- owl.carousel -->
+		<script src="asset/js/owl.carousel.min.js"></script>
+		<!-- js count to -->
+		<script src="asset/js/jquery.appear.js"></script>
+		<script src="asset/js/jquery.countTo.js"></script>
+		<!-- Fancybox -->
+		<script src="asset/js/jquery.fancybox.min.js"></script>
+
+		<!-- Language js -->
+		<script src="https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+
+
+		<!-- Theme js -->
+		<script src="asset/js/lang.js"></script>
+		<script src="asset/js/theme.js"></script>
+		</div> <!-- /.main-page-wrapper -->
+		<style>
+		
+		body.modal-open {
+			overflow: hidden !important;
+		}
+		.filter-lightblue{
+			filter: invert(45%) sepia(80%) saturate(3050%) hue-rotate(165deg) brightness(100%) contrast(103%);
+		}
+		
+		.fade{
+			transition: opacity .15s linear !important;
+		}
+		.modal-dialog{
+			max-width: 800px;
+		}
+		</style>
+		<div id="myModal" class="modal fade" role="dialog"  data-backdrop="static" data-keyboard="false">
+			<div class="modal-dialog" style='align:center'>
+				<div class="modal-content" style='height:400px;'>
+					<div class="modal-header">
+						<h5>Informasi</h5>
+						<button type="button" class="close" data-dismiss="modal">&times;</button>
+					</div>
+					<div class="modal-body" style="background-color: #fff;">
+						<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+							<div class="carousel-inner">
+																		<div class="carousel-item active">
+											<img class="d-block w-100" src="asset/BERAKHLAK.webp" alt="Logo BerAKHLAK">
+										</div>
+																		<div class="carousel-item ">
+											<img class="d-block w-100" src="asset/77th.png" alt="17 Agustus 2022">
+										</div>
+									
+							</div>
+							<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+								<span class="carousel-control-prev-icon filter-lightblue" aria-hidden="true"></span>
+								<span class="sr-only">Previous</span>
+							</a>
+							<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+								<span class="carousel-control-next-icon filter-lightblue" aria-hidden="true"></span>
+								<span class="sr-only">Next</span>
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<script type="text/javascript">
+		$(document).ready(function(){
+							var delayMs = 3000;
+				setTimeout(function(){
+					$('#myModal').modal('show');
+				}, delayMs);
+					});
+		</script>
+	</body>
+</html>
